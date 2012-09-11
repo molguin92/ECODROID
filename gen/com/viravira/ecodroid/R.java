34d90b0be1f42e3293c845c4668c5d3204d63f95
Manuel Olguin <manuel.olguin@ug.uchile.cl>
2012-09-11 00:28:01 -0300

    Estuve experimentando un poco con los layouts de las actividades.

--- gen/com/viravira/ecodroid/R.java
+++ gen/com/viravira/ecodroid/R.java
@@ -16,8 +16,16 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int ecodroidlogo=0x7f070000;
-        public static final int menu_settings=0x7f070001;
+        public static final int BtnPlay=0x7f070007;
+        public static final int BtnRec=0x7f070004;
+        public static final int ecodroidlogo=0x7f070002;
+        public static final int menu_settings=0x7f070009;
+        public static final int spacer1=0x7f070000;
+        public static final int spacer2=0x7f070001;
+        public static final int spacerh1=0x7f070003;
+        public static final int spacerh2=0x7f070005;
+        public static final int spacerh3=0x7f070006;
+        public static final int spacerh4=0x7f070008;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -26,6 +34,8 @@ public final class R {
         public static final int activity_main=0x7f060000;
     }
     public static final class string {
+        public static final int BtnPlay=0x7f040006;
+        public static final int BtnRec=0x7f040005;
         public static final int app_name=0x7f040000;
         public static final int ecodroidlogo=0x7f040004;
         public static final int hello_world=0x7f040001;
