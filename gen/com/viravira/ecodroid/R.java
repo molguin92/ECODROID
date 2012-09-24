762c40d45a5411a6e37760f6bff1bf35ddeb1b82
Arachnid92 <manuel.olguin@ug.uchile.cl>
2012-09-24 19:28:47 -0300

    Cambios varios en la interfaz - Se elimina la actividad de mapas y se agregan actividades para distintas instancias del juego

--- gen/com/viravira/ecodroid/R.java
+++ gen/com/viravira/ecodroid/R.java
@@ -21,19 +21,25 @@ public final class R {
         public static final int icono_reciclar=0x7f020007;
     }
     public static final class id {
-        public static final int BtnPlay=0x7f070009;
-        public static final int BtnRec=0x7f070005;
-        public static final int ListRecCat=0x7f07000b;
+        public static final int BtnContinue=0x7f07000a;
+        public static final int BtnNewGame=0x7f070006;
+        public static final int BtnSettings=0x7f07000e;
+        public static final int ListRecCat=0x7f070011;
         public static final int ecodroidlogo=0x7f070001;
-        public static final int menu_settings=0x7f07000c;
+        public static final int menu_settings=0x7f070012;
         public static final int spacer1=0x7f070000;
         public static final int spacer2=0x7f070002;
-        public static final int spacerh1=0x7f070004;
-        public static final int spacerh2=0x7f070006;
-        public static final int spacerh3=0x7f070008;
-        public static final int spacerh4=0x7f07000a;
-        public static final int tableRow1=0x7f070003;
-        public static final int tableRow2=0x7f070007;
+        public static final int spacerh1=0x7f070005;
+        public static final int spacerh2=0x7f070007;
+        public static final int spacerh3=0x7f070009;
+        public static final int spacerh4=0x7f07000b;
+        public static final int spacerh5=0x7f07000d;
+        public static final int spacerh6=0x7f07000f;
+        public static final int spacervert1=0x7f070003;
+        public static final int spacervert2=0x7f070010;
+        public static final int tableRow1=0x7f070004;
+        public static final int tableRow2=0x7f070008;
+        public static final int tableRow3=0x7f07000c;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -47,16 +53,19 @@ public final class R {
         public static final int activity_uno=0x7f060003;
     }
     public static final class string {
+        public static final int BtnContinue=0x7f040008;
+        public static final int BtnNewGame=0x7f040007;
         public static final int BtnPlay=0x7f040006;
         public static final int BtnRec=0x7f040005;
+        public static final int BtnSettings=0x7f040009;
         public static final int app_name=0x7f040000;
         public static final int ecodroidlogo=0x7f040004;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
         public static final int title_activity_main=0x7f040003;
-        public static final int title_activity_play=0x7f040009;
-        public static final int title_activity_rec=0x7f040007;
-        public static final int title_activity_uno=0x7f040008;
+        public static final int title_activity_play=0x7f04000c;
+        public static final int title_activity_rec=0x7f04000a;
+        public static final int title_activity_uno=0x7f04000b;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
