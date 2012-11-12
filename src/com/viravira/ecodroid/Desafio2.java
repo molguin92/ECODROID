46d6c22e4dde22f3f75b0672a815a6515ccaa529
Arachnid92 <manuel.olguin@ug.uchile.cl>
2012-11-12 04:04:53 +0000

    Se arregla un error con la implementación del código para activar/desactivar el botón Completado.

--- src/com/viravira/ecodroid/Desafio2.java
+++ src/com/viravira/ecodroid/Desafio2.java
@@ -24,7 +24,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class Desafio2 extends Activity {
-	
+
 	public String puntaje;
 	public String numdesafio;
 	public int dias;
@@ -32,37 +32,32 @@ public class Desafio2 extends Activity {
 	private ListView checkboxes;
 	private String[] arr;
 	public int counter;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_desafio2);
-		
+
 		// A continuación se leen los datos guardados en las preferencias
 		// de la app, y se carga el desafío correspondiente.
-		// Luego, se actualizan los TextViews correspondientes con los 
+		// Luego, se actualizan los TextViews correspondientes con los
 		// datos guardados.
 
 		TextView tviewtitulo = (TextView) findViewById(R.id.titulodes2);
 		TextView tviewdesafio = (TextView) findViewById(R.id.textodes2);
 		TextView tviewpuntaje = (TextView) findViewById(R.id.puntajedes2);
-		Button completado = (Button)findViewById(R.id.btncompletado2);
-		if(dias == checkboxes.getCheckItemIds().length){
-			completado.setEnabled(true);
-		} else {
-			completado.setEnabled(false);
-		}
-		
+		Button completado = (Button) findViewById(R.id.btncompletado2);
+
 		Intent intent = getIntent();
 		int num = intent.getIntExtra("num", 1);
-		if (num==1){
+		if (num == 1) {
 			numdesafio = "desafio1";
-		}else if(num==2){
+		} else if (num == 2) {
 			numdesafio = "desafio2";
-		}else{
+		} else {
 			numdesafio = "desafio3";
 		}
-		
+
 		try {
 
 			BufferedReader archivodesafios = new BufferedReader(
@@ -76,81 +71,93 @@ public class Desafio2 extends Activity {
 
 			SharedPreferences desafios = Desafio2.this.getApplicationContext()
 					.getSharedPreferences("Prefs", 0);
-			Random desafiosazar = new Random(); 
-			
-			int desafioint = desafios.getInt(numdesafio, desafiosazar.nextInt(40));
-			
+			Random desafiosazar = new Random();
+
+			int desafioint = desafios.getInt(numdesafio,
+					desafiosazar.nextInt(40));
+
 			// El siguiente comando "for" es para saltarse el número
 			// de líneas necesario para llegar a la línea del desafío.
-			for(int i = 0; i < desafioint; i++){
+			for (int i = 0; i < desafioint; i++) {
 				archivodesafios.readLine();
 				archivopuntajes.readLine();
 				archivotitulos.readLine();
 				archivodias.readLine();
 			}
-			
+
 			String texto = archivodesafios.readLine();
 			String titulo = archivotitulos.readLine();
 			dias = Integer.parseInt(archivodias.readLine());
 			puntaje = archivopuntajes.readLine();
-			
+
 			tviewdesafio.setText(texto);
 			tviewtitulo.setText(titulo);
 			tviewpuntaje.setText(puntaje + getString(R.string.pts));
-			
-			//Generar array con los textos de los Checkboxes
-			
+
+			// Generar array con los textos de los Checkboxes
+
 			arr = new String[dias];
-			for (int i = 1; i <= dias; i++){
-				arr[i-1] = "Día " + i;
+			for (int i = 1; i <= dias; i++) {
+				arr[i - 1] = "Día " + i;
 			}
-			
+
 			// Agregar los checkboxes correspondientes al ListView
-			
+
 			checkboxes = (ListView) findViewById(R.id.ChkboxList);
 			checkboxes.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-			checkboxes.setAdapter(new ArrayAdapter<Object>(this, 
+			checkboxes.setAdapter(new ArrayAdapter<Object>(this,
 					android.R.layout.simple_list_item_multiple_choice, arr));
-			checkboxes.setOnItemClickListener(new OnItemClickListener() {
-				
-				public void onItemClick(AdapterView<?> arg0, View arg1,
-						int arg2, long arg3) {
-					
-					if(dias == checkboxes.getCheckItemIds().length){
-						Button completado = (Button)findViewById(R.id.btncompletado2);
-						completado.setEnabled(true);
-					}
-					
-				}
-			});
-		
-			//Recuperar estado de los checkboxes 
-			//TODO Bloquear checkboxes en el "futuro", que no pueden ser realizados todavia
 			
-			boolean state;
-						
-			for (int i=1;i<=dias;i++){
-				state = desafios.getBoolean(desafioint + "_" + i, false);
-				checkboxes.setItemChecked(i-1, state);
+			if (dias == checkboxes.getCheckItemIds().length) {
+				completado.setEnabled(true);
+			} else {
+				completado.setEnabled(false);
 			}
 			
-//			
-//			CheckBox check1 = (CheckBox) findViewById(R.id.checkbox1);
-//			CheckBox check2 = (CheckBox) findViewById(R.id.checkbox2);
-//			
-//			String desafiocheck1 = desafioint + "_" + "1";
-//			String desafiocheck2 = desafioint + "_" + "2";
-//			
-//			boolean descheck1 = desafios.getBoolean(desafiocheck1, false);
-//			boolean descheck2 = desafios.getBoolean(desafiocheck2, false);
-//			
-//			if(descheck1){
-//				check1.setChecked(true);
-//			}
-//			if(descheck2){
-//				check2.setChecked(true);
-//			}
-//			
+			checkboxes.setOnItemClickListener(new OnItemClickListener() {
+
+				public void onItemClick(AdapterView<?> arg0, View arg1,
+						int arg2, long arg3) {
+
+					Button completado = (Button) findViewById(R.id.btncompletado2);
+
+					if (dias == checkboxes.getCheckItemIds().length) {
+						completado.setEnabled(true);
+					} else {
+						completado.setEnabled(false);
+					}
+
+				}
+			});
+
+			// Recuperar estado de los checkboxes
+			// TODO Bloquear checkboxes en el "futuro", que no pueden ser
+			// realizados todavia
+
+			boolean state;
+
+			for (int i = 1; i <= dias; i++) {
+				state = desafios.getBoolean(desafioint + "_" + i, false);
+				checkboxes.setItemChecked(i - 1, state);
+			}
+
+			//
+			// CheckBox check1 = (CheckBox) findViewById(R.id.checkbox1);
+			// CheckBox check2 = (CheckBox) findViewById(R.id.checkbox2);
+			//
+			// String desafiocheck1 = desafioint + "_" + "1";
+			// String desafiocheck2 = desafioint + "_" + "2";
+			//
+			// boolean descheck1 = desafios.getBoolean(desafiocheck1, false);
+			// boolean descheck2 = desafios.getBoolean(desafiocheck2, false);
+			//
+			// if(descheck1){
+			// check1.setChecked(true);
+			// }
+			// if(descheck2){
+			// check2.setChecked(true);
+			// }
+			//
 			SharedPreferences.Editor editor = desafios.edit();
 			editor.putInt(numdesafio, desafioint);
 			editor.commit();
@@ -195,146 +202,143 @@ public class Desafio2 extends Activity {
 			System.exit(1);
 		}
 
-
-
 	}
-	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.activity_desafio2, menu);
 		return true;
 	}
-	
-	public void Completado (View view){
-	
+
+	public void Completado(View view) {
+
 		// Este método describe el botón "Completado".
 		// Aquí se genera un nuevo desafío al completar
-		// el anterior, y se actualiza la información 
+		// el anterior, y se actualiza la información
 		// guardada en SharedPreferences.
-		// También se actualiza el puntaje total del 
+		// También se actualiza el puntaje total del
 		// usuario (se suma el ptje anterior con el
 		// puntaje asociado al desafío en cuestión.
-		
-//		CheckBox check1 = (CheckBox) findViewById(R.id.checkbox1);
-//		CheckBox check2 = (CheckBox) findViewById(R.id.checkbox2);
-		
+
+		// CheckBox check1 = (CheckBox) findViewById(R.id.checkbox1);
+		// CheckBox check2 = (CheckBox) findViewById(R.id.checkbox2);
+
 		Intent intent = getIntent();
 		int num = intent.getIntExtra("num", 1);
-		
-		if (num==1){
+
+		if (num == 1) {
 			numdesafio = "desafio1";
-		}else if(num==2){
+		} else if (num == 2) {
 			numdesafio = "desafio2";
-		}else{
+		} else {
 			numdesafio = "desafio3";
 		}
-		
+
 		Random desafiosazar = new Random();
-		
+
 		Context context = Desafio2.this.getApplicationContext();
 
-		SharedPreferences desafios = context.getSharedPreferences("Prefs",
-						0);
-		
-//		//Detectar que todos los checkboxes esten marcados
-//		for (int i = 0; i <= checkboxes.getChildCount();i++){
-//			if (!checkboxes.isItemChecked(i)){
-//				int duration = Toast.LENGTH_SHORT;
-//				Toast toast = Toast.makeText(context, "Para completar el desafío correctamente debes marcar TODOS los checkboxes", duration);
-//				toast.show();
-//				return;
-//			}
-//		}
-		
+		SharedPreferences desafios = context.getSharedPreferences("Prefs", 0);
+
+		// //Detectar que todos los checkboxes esten marcados
+		// for (int i = 0; i <= checkboxes.getChildCount();i++){
+		// if (!checkboxes.isItemChecked(i)){
+		// int duration = Toast.LENGTH_SHORT;
+		// Toast toast = Toast.makeText(context,
+		// "Para completar el desafío correctamente debes marcar TODOS los checkboxes",
+		// duration);
+		// toast.show();
+		// return;
+		// }
+		// }
+
 		int desafioint = desafios.getInt(numdesafio, desafiosazar.nextInt(40));
-		int puntajetotal = Integer.parseInt(puntaje) + desafios.getInt("puntaje", 0);
+		int puntajetotal = Integer.parseInt(puntaje)
+				+ desafios.getInt("puntaje", 0);
 		SharedPreferences.Editor editor = desafios.edit();
-				
-		for (int i=1;i<=dias;i++){
+
+		for (int i = 1; i <= dias; i++) {
 			editor.remove(desafioint + "_" + i);
 		}
-		
-//		String desafiocheck1 = desafioint + "_" + "1";
-//		String desafiocheck2 = desafioint + "_" + "2";
-		
 
-		
-//		if(check1.isChecked()){
-//			editor.putBoolean(desafiocheck1, false);
-//		}
-//		if(check2.isChecked()){
-//			editor.putBoolean(desafiocheck2, false);
-//		}
-		
+		// String desafiocheck1 = desafioint + "_" + "1";
+		// String desafiocheck2 = desafioint + "_" + "2";
+
+		// if(check1.isChecked()){
+		// editor.putBoolean(desafiocheck1, false);
+		// }
+		// if(check2.isChecked()){
+		// editor.putBoolean(desafiocheck2, false);
+		// }
+
 		editor.putInt(numdesafio, desafiosazar.nextInt(40));
 		editor.putInt("puntaje", puntajetotal);
 
 		editor.commit();
-		
+
 		int duration = Toast.LENGTH_SHORT;
-		Toast toast = Toast.makeText(context, getString(R.string.youvewon) + " " + puntaje + " "+getString(R.string.pts)+
-				"!", duration);
-		
+		Toast toast = Toast
+				.makeText(context, getString(R.string.youvewon) + " " + puntaje
+						+ " " + getString(R.string.pts) + "!", duration);
+
 		toast.show();
-		
+
 		Desafio2.this.finish();
-		
+
 	}
-	
-	public void Cancelar (View view){
-		
-//		CheckBox check1 = (CheckBox) findViewById(R.id.checkbox1);
-//		CheckBox check2 = (CheckBox) findViewById(R.id.checkbox2);
-		
+
+	public void Cancelar(View view) {
+
+		// CheckBox check1 = (CheckBox) findViewById(R.id.checkbox1);
+		// CheckBox check2 = (CheckBox) findViewById(R.id.checkbox2);
+
 		Intent intent = getIntent();
-		
+
 		int num = intent.getIntExtra("num", 1);
-		
-		if (num==1){
+
+		if (num == 1) {
 			numdesafio = "desafio1";
-		}else if(num==2){
+		} else if (num == 2) {
 			numdesafio = "desafio2";
-		}else{
+		} else {
 			numdesafio = "desafio3";
 		}
-		
+
 		Context context = Desafio2.this.getApplicationContext();
 
-		SharedPreferences desafios = context.getSharedPreferences("Prefs",
-						0);
-		
-		Random desafiosazar = new Random(); 
-		
+		SharedPreferences desafios = context.getSharedPreferences("Prefs", 0);
+
+		Random desafiosazar = new Random();
+
 		int desafioint = desafios.getInt(numdesafio, desafiosazar.nextInt(40));
-		
-//		String desafiocheck1 = desafioint + "_" + "1";
-//		String desafiocheck2 = desafioint + "_" + "2";
-		
+
+		// String desafiocheck1 = desafioint + "_" + "1";
+		// String desafiocheck2 = desafioint + "_" + "2";
+
 		SharedPreferences.Editor editor = desafios.edit();
-		
-		for (int i=1;i<=dias;i++){
-			if (checkboxes.isItemChecked(i-1)){
+
+		for (int i = 1; i <= dias; i++) {
+			if (checkboxes.isItemChecked(i - 1)) {
 				editor.putBoolean(desafioint + "_" + i, true);
-			}else{
+			} else {
 				editor.putBoolean(desafioint + "_" + i, false);
 			}
 		}
-		
-//		if(check1.isChecked()){
-//			editor.putBoolean(desafiocheck1, true);
-//		}
-//		if(check2.isChecked()){
-//			editor.putBoolean(desafiocheck2, true);
-//		}
 
-		editor.commit();		
-		
+		// if(check1.isChecked()){
+		// editor.putBoolean(desafiocheck1, true);
+		// }
+		// if(check2.isChecked()){
+		// editor.putBoolean(desafiocheck2, true);
+		// }
+
+		editor.commit();
+
 		Desafio2.this.finish();
-		
+
 	}
-	
-	protected void onStop(){
+
+	protected void onStop() {
 		super.onStop();
 		Cancelar(findViewById(R.id.btncancelar2));
 	}
